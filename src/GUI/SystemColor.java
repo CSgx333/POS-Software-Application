f93df7b99acd5fa04c18d46dd75b2860f84d9c92
Chaiyakorn Salakhong <108126058+CSgx333@users.noreply.github.com>
2023-04-17 13:43:01 +0700

    Add files via upload

---
+++ src/GUI/SystemColor.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import java.awt.Color;
+
+public class SystemColor {
+
+    public final static Color MAIN_COLOR_1 = Color.decode("#FEAC5E");
+    public final static Color MAIN_COLOR_2 = Color.decode("#C779D0");
+    public final static Color MAIN_COLOR_3 = Color.decode("#4BC0C8");
+}
